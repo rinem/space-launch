29a5a7793bea920a17e32958789771ca675b30c1
Roshan Mishra <roshanmishrarinem@gmail.com>
2019-09-15 21:13:23 +0530

    Add Comments Section

---
+++ src/comp/stateless/comments.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class comments extends Component {
+  render() {
+    return (
+      <div>
+        <div class="container">
+            <div id="comments"></div>
+        </div>
+      </div>
+    );
+  }
+}
